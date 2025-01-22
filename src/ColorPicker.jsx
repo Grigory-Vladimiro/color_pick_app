6072d6358bb0c65c02c19649047e7c26e48d7a99
Grigory-Vladimiro <grigori.vladimiro@gmail.com>
2025-01-22 14:36:12 +0100

    color-picker.jsx - 4.0

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -2,8 +2,15 @@ import React, {useState} from 'react';
 
 function ColorPicker(){
 
-    const [color, setColor] = useState("#FFFFFF");
+    const [color, setColor] = useState({hue: 0, saturation: 50, lighness: 50});
 
-    return<></>
+    return(<div className="color-picker-container">
+<h1>Color Picker</h1>
+<div className="color-display" style={{backgroundColor: color}}>
+<p>Selected Color: {color}</p>
+    </div>
+    <label>Select a color:</label>
+    <input></input>
+    </div>);
 } 
 export default ColorPicker
